b44e7623351429859c799dff4c075ab1a8e07c7c
brendasantana04 <brenda.santos33@fatec.sp.gov.br>
2023-12-12 10:13:28 -0300

    Desafio de Código - Explorando Sintaxe e Tipos de Dados

---
+++ explorando-sintaxe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // TODO: Declare as variáveis para guardar as informações de nome, email e senha:
+        string registroEmail;
+        string registroNome;
+        string registroSenha;
+
+        // Obtém o email e nome do usuário a partir da entrada do console
+        registroEmail = Console.ReadLine();
+        registroNome = Console.ReadLine();
+        registroSenha = Console.ReadLine();
+
+        // TODO: Imprima a mensagem formatada com o nome do usuário e o email de registro:
+        Console.WriteLine($"{registroNome}, verifique o email: {registroEmail} para ativar.");
+    }
+}
